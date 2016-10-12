46b750a78648f249bfdc3a39afaffde14e4d8a09
Koo294 <leoleoly@aol.com>
2016-10-12 21:37:01 +0100

    HUD target arrows

---
+++ Source/Sunlit/Public/ShipHUDWidget.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "ShipHUDWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUNLIT_API UShipHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/Sunlit/Public/ShipTargetWidget.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "ShipTargetWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUNLIT_API UShipTargetWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
